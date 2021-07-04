358831035732460ced4bd643b6bc7332931cae0f
Hao Wu <hao.wu.au@gmail.com>
2021-07-04 23:54:16 +1000

    Add / remove rung

--- ladder.js
+++ ladder.js
@@ -1,7 +1,32 @@
 var jsonObject;
 
-function loadFileAsJson() {
-    var fileToLoad = document.getElementById("json_file").files[0];
+var dragged;
+
+const x = ['X0', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9'];
+const y = ['Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5'];
+const m = ['M0', 'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9'];
+
+const rungTemplate =    '<div class="rung" draggable="true">' +
+                            '<div class="short-wire">' +
+                                '<div class="wire-visible"></div>' +
+                            '</div>' +
+                            '<div class="wire">' +
+                                '<div class="wire-visible"></div>' +
+                            '</div>' +
+                            '<div class="short-wire">' +
+                                '<div class="wire-visible"></div>' +
+                            '</div>' +
+                        '</div>';
+
+const xicTemplate = 
+
+async function readFromJson() {
+    $("#json_file").trigger("click");
+    do {
+        var fileToLoad = document.getElementById("json_file").files[0];
+        // console.log(fileToLoad);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    } while(fileToLoad === undefined)
 
     var fileReader = new FileReader();
     fileReader.onload = function(fileLoadedEvent){
@@ -13,15 +38,37 @@ function loadFileAsJson() {
     fileReader.readAsText(fileToLoad, "UTF-8");
 }
 
-function readFromJson() {
-
-}
-
 function saveToJson() {
 
 }
 
+function addRung() {
+    $(".rungs").append(rungTemplate);
+}
+
+$(document).on("dragstart", "div[draggable=true]", function(event) {
+    dragged = event.target;
+});
+
+$(".trash").on("dragenter", function(event) {
+    $(this).css("outline", "2px dashed blue");
+});
+
+$(".trash").on("dragleave", function(event) {
+    $(this).css("outline", "2px dashed #5c5c5c");
+});
+
+$(".trash").on("dragover", function(event) {
+    event.preventDefault();
+});
+
+$(".trash").on("drop", function(event) {
+    console.log(event);
+    dragged.remove();
+    dragged = null;
+});
+
 $.ready(function() {
-    
+
 });
 
