1b3b1903611d38919d95c0f645e2d22d43dd7cb0
Hao Wu <hao.wu.au@gmail.com>
2021-07-06 00:10:02 +1000

    Drag and Drop

--- ladder.js
+++ ladder.js
@@ -6,7 +6,7 @@ const x = ['X0', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9'];
 const y = ['Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5'];
 const m = ['M0', 'M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8', 'M9'];
 
-const rungTemplate =    '<div class="rung" draggable="true">' +
+const rungTemplate =    '<div class="rung rung-template" draggable="true">' +
                             '<div class="short-wire">' +
                                 '<div class="wire-visible"></div>' +
                             '</div>' +
@@ -18,7 +18,88 @@ const rungTemplate =    '<div class="rung" draggable="true">' +
                             '</div>' +
                         '</div>';
 
-const xicTemplate = 
+const xicTemplate =     '<div class="ladder-element xic-template" draggable="true">' +
+                            '<div class="ladder-element-bool">' +
+                                '<select class="variable-select" disabled="">' +
+                                    '<option value="" disabled="" hidden="">Variable</option>' +
+                                '</select>' +
+                                '<div class="circuit-element">' +
+                                    '<div class="wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                    '<div class="xic"></div>' +
+                                    '<div class="wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+
+const xioTemplate =     '<div class="ladder-element xio-template" draggable="true">' +
+                            '<div class="ladder-element-bool">' +
+                                '<select class="variable-select" disabled="">' +
+                                    '<option value="" disabled="" hidden="">Variable</option>' +
+                                '</select>' +
+                                '<div class="circuit-element">' +
+                                    '<div class="wire"><div class="wire-visible"></div>' +
+                                '</div>' +
+                                '<div class="xio"></div>' +
+                                '<div class="wire">' +
+                                    '<div class="wire-visible"></div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+
+const oteTemplate =     '<div class="ladder-element ote-template" draggable="true">' +
+                            '<div class="ladder-element-bool">' +
+                                '<select class="variable-select" disabled="">' +
+                                    '<option value="" disabled="" hidden="">Variable</option>' +
+                                '</select><div class="circuit-element">' +
+                                    '<div class="wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                    '<div class="ote"></div>' +
+                                    '<div class="wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>';
+                        
+const branchTemplate =  '<div class="branch-logic branch-template" draggable="true">' +
+                            '<div class="short-wire">' +
+                                '<div class="wire-visible"></div>' +
+                            '</div>' +
+                            '<div class="branch-vertical" style="height: 58px;"></div>' +
+                            '<div class="branch-rungs">' +
+                                '<div class="rung">' +
+                                    '<div class="short-wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                    '<div class="wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                    '<div class="short-wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                '</div>' +
+                                '<div class="rung">' +
+                                    '<div class="short-wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                    '<div class="wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                    '<div class="short-wire">' +
+                                        '<div class="wire-visible"></div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="branch-vertical" style="height: 58px;"></div>' +
+                            '<div class="short-wire">' +
+                                '<div class="wire-visible"></div>' +
+                            '</div>' +
+                        '</div>';
 
 async function readFromJson() {
     $("#json_file").trigger("click");
@@ -50,6 +131,8 @@ $(document).on("dragstart", "div[draggable=true]", function(event) {
     dragged = event.target;
 });
 
+// Move objects to the trash
+
 $(".trash").on("dragenter", function(event) {
     $(this).css("outline", "2px dashed blue");
 });
@@ -66,8 +149,55 @@ $(".trash").on("drop", function(event) {
     console.log(event);
     dragged.remove();
     dragged = null;
+    $(this).css("outline", "2px dashed #5c5c5c");
 });
 
+// Move objects to the branch
+
+$(".logic .branch-logic .branch-rungs .rung .wire").on("dragenter", function(event) {
+    $(this).css("outline", "2px dashed blue");
+});
+
+$(".logic .branch-logic .branch-rungs .rung .wire").on("dragleave", function(event) {
+    $(this).css("outline", "");
+});
+
+$(".logic .branch-logic .branch-rungs .rung .wire").on("dragover", function(event) {
+    event.preventDefault();
+});
+
+$(".logic .branch-logic .branch-rungs .rung .wire").on("drop", function(event) {
+    var cloned = dragged.cloneNode(true);
+    $(this).before(cloned);
+    dragged = null;
+    $(this).css("outline", "");
+});
+
+// Move objects to the rung
+$(".logic > .rungs > .rung > .wire").on("dragenter", function(event) {
+    $(this).css("outline", "2px dashed blue");
+});
+
+$(".logic > .rungs > .rung > .wire").on("dragleave", function(event) {
+    $(this).css("outline", "");
+});
+
+$(".logic > .rungs > .rung > .wire").on("dragover", function(event) {
+    event.preventDefault();
+});
+
+$(".logic > .rungs > .rung > .wire").on("drop", function(event) {
+    var cloned = dragged.cloneNode(true);
+    if($(cloned).hasClass("ote-template")) {
+        $(this).after(cloned);
+    } else {
+        $(this).before(cloned);
+    }
+    dragged = null;
+    $(this).css("outline", "");
+});
+
+// Document ready
 $.ready(function() {
 
 });
